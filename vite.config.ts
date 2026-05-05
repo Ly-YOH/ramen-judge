64ab6657b1b98a951f17c4e09cbd282ee166bccb
Ly-YOH <sachitomokiichu@gmail.com>
2026-05-05 16:09:04 +0900

    Viteビルド後にAdMaxタグを確実に埋め込む

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-admax',
+      transformIndexHtml(html) {
+        return html.replace(
+          '</body>',
+          `<!-- admax -->\n<script src="https://adm.shinobi.jp/s/45f785f2c648050ded30ac3f29866b46"></script>\n<!-- admax -->\n</body>`
+        )
+      },
+    },
+  ],
 })
